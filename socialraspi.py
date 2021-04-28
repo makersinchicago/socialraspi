a48f5efbf4834f9316553bd8c8530951f99a5545
Jorge Garcia <jorge@jorgegarcia.io>
2021-04-28 16:13:54 -0500

    created socialraspi.py
    avrec overwrites main and main is renamed to socialraspi. avrec is remove and main is recreated as a placeholder for calling socialraspi

--- socialraspi.py
+++ socialraspi.py
@@ -1,3 +1,21 @@
+"""
+
+at boot, present prompt
+
+wait for user input
+
+at user input, record 1 minute of video and audio using picam
+
+after recording is done, present prompt. user may play back video or record anew
+
+if the user is satisfied, they may press send to post the video to makersinchicago's twitter timeline, the chimakerfest's ig feed, and the chimakerfest facebook page.
+
+the is notified that the posts have been uploaded and the prompt is presented for the next user.
+
+there is a fourth button to enable a safe shutdown, it is located near the power switch.
+
+"""
+
 import alsaaudio, wave, numpy
 import os
 from picamera import PiCamera
